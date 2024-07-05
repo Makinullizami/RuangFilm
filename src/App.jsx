30e27260f83448fe54aa48db42a25f9dcaef3df8
Makinullizami <agimbal65@gmail.com>
2024-07-06 01:14:44 +0700

    membuat header home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './screens/Home';
 import Detail from './screens/Detail';
 import Live from './screens/Live';
@@ -13,16 +14,51 @@ const Tab = createBottomTabNavigator();
 
 const Menu = () => {
   return (
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#e91e63',
-        }}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Detail" component={Detail} />
-        <Tab.Screen name="Live" component={Live} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-    
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Live"
+        component={Live}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 const App = () => {
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,84 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Home = () => {
+  const Kategori = [
+    {key: 'Home', label: 'Home'},
+    {key: 'Series', label: 'Series'},
+    {key: 'Movies', label: 'Movies'},
+    {key: 'Anime', label: 'Anime'},
+    {key: 'Sports', label: 'Sports'},
+    {key: 'TV Shows', label: 'TV Shows'},
+  ];
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#333" barStyle={'light-content'} />
+      <View style={styles.headerTop}>
+        <Text style={styles.logo}>RuangFilm</Text>
+        <View style={styles.iconsContainer}>
+          <MaterialCommunityIcons name="magnify" size={28} color="#fff" />
+          <MaterialCommunityIcons
+            name="account-circle"
+            size={28}
+            color="#fff"
+          />
+        </View>
+      </View>
+      <FlatList
+        data={Kategori}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.tab}>
+            <Text style={styles.tabLabel}>{item.label}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.key}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#333',
+  },
+  headerTop: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    elevation: 1,
+  },
+  logo: {
+    fontSize: 30,
+    color: '#ff0000',
+    fontWeight: 'bold',
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+  },
+  tab: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginHorizontal: 10,
+    backgroundColor: '#222',
+    borderRadius: 20,
+    marginBottom: 8,
+    marginTop: 10,
+  },
+  tabLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
